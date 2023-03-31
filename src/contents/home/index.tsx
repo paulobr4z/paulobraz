83b53d65fc10cf31418b53e0e52cf4ad1c57811d
paulobr4z <paulobrazaraujo@gmail.com>
2023-03-31 17:14:18 -0300

    add projects section

--- src/contents/home/index.tsx
+++ src/contents/home/index.tsx
@@ -2,7 +2,7 @@ import { GoToTop } from "@/components/GoToTop";
 import { Header } from "@/components/Header";
 import { Skills } from "@/constants";
 import { ArrowUpRight } from "phosphor-react";
-import { ContactSection, HomeContainer, MainContent, ProjectsSection, Section, SkillsContent, SkillsSection, Wrapper } from "./styles";
+import { ContactSection, ExperienceSection, HomeContainer, MainContent, ProjectsSection, Section, SkillsContent, SkillsSection, Wrapper } from "./styles";
 
 export function HomeContent() {
   return (
@@ -14,24 +14,69 @@ export function HomeContent() {
             <video src="/videos/bg-video.mp4" playsInline autoPlay muted loop />
           </div>
           <div className="title">
-            <p>What’s up?</p>
+            <p>What&lsquo;s up?</p>
             <p>I am Paulo Braz</p>
             <p>Full-Stack Developer</p>
           </div>
         </MainContent>
       </Section>
       <ProjectsSection id="projects">
+        <div className="projects-title">
+          <h2>Projects</h2>
+        </div>
         <div className="wrapper">
-          <div className="unilink">
-            <p>projects</p>
-          </div>
+          <div className="project"></div>
+          <div className="project"></div>
+          <div className="project"></div>
+          <div className="project"></div>
+          <div className="project"></div>
+          <div className="project"></div>
+          <div className="project"></div>
+          <div className="project"></div>
         </div>
       </ProjectsSection>
-      <Section id="experience">
-        <Wrapper>
-          <p>experience</p>
-        </Wrapper>
-      </Section>
+      <ExperienceSection id="experience">
+        <div className="wrapper">          
+          <h2>Experience</h2>
+          <div className="experiences">
+            <span>
+              <h3>WEDIGI</h3>
+              <h4>Desenvolvedor Front-end Jr</h4>
+              <ul>
+                <li>
+                  Wedigi tech: Equipe responsável por criar soluções de implementação e evolução 
+                  com objetivo de manter o e-commerce dos clientes atualizado.
+                </li>
+                <li>
+                  Consultoria para entender o plano de negocios do cliente, 
+                  adicionando as melhores soluções e novas funcionalidades solicitada.
+                </li>
+                <li>
+                  Tecnologia utilizadas: React, JavaScript, Vtex, VTEX IO, TypeScript, CSS, SCSS, HTML.
+                </li>
+              </ul>
+            </span>
+            <span>
+              <h3>ADBAT</h3>
+              <h4>Desenvolvedor Front-end Jr</h4>
+              <ul>
+                <li>Desenvolvimento de um sistema web para uma corretora de imoveis.</li>
+                <li>
+                  Desenvolvimento de layouts responsivos, garantindo que o sistema se adapte de 
+                  forma adequada em diferentes tamanhos de tela e dispositivos, proporcionando uma melhor 
+                  experiência para o usuário final.
+                </li>
+                <li>
+                  Criação e Validação de formulários, validar os dados inseridos pelo usuário para evitar 
+                  erros e inconsistências no sistema.
+                </li>
+                <li>Consumo de API, seja para integração de sistemas ou para buscar informações em tempo real.</li>
+                <li> ReactJS, NextJs, TypeScript, Styled Components.</li>
+              </ul>
+            </span>
+          </div>
+        </div>
+      </ExperienceSection>
       <SkillsSection id="skills">
         <div className="wrapper">
           <h2>Skills</h2>
@@ -49,7 +94,7 @@ export function HomeContent() {
       </SkillsSection>
       <ContactSection id="contact">
         <div className="wrapper">
-          <h2>Let's start a project together?</h2>
+          <h2>Let&lsquo;s start a project together?</h2>
           <a href="https://www.linkedin.com/in/paulobr4z/" target="_blank" rel="noreferrer">
             <span>
               <p>LINKEDIN</p>
--- src/contents/home/styles.ts
+++ src/contents/home/styles.ts
@@ -12,7 +12,7 @@ export const HomeContainer = styled.div`
   justify-content: center;
   flex-direction: column;
   width: 100%;
-`
+`;
 
 export const Section = styled.section<ISection>`
   display: flex;
@@ -79,6 +79,21 @@ export const MainContent = styled.div`
       width: 100%;
       text-align: center;
       line-height: 8vw;
+
+      background: #FF416C;
+      background: -webkit-linear-gradient(to right, #FF4B2B, #FF416C);
+      background: linear-gradient(to right, #FF4B2B, #FF416C);
+
+      background: #8A2387;
+      background: -webkit-linear-gradient(to right, #F27121, #E94057, #8A2387);
+      background: linear-gradient(to right, #F27121, #E94057, #8A2387);
+
+      background: #59C173;
+      background: -webkit-linear-gradient(to right, #5D26C1, #a17fe0, #59C173);
+      background: linear-gradient(to right, #5D26C1, #a17fe0, #59C173);
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
     }
   }
 `;
@@ -87,17 +102,125 @@ export const ProjectsSection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   width: 100%;
   background-color: #09090B;
+  padding: 120px 32px;
+
+  @media (max-width: 800px) {
+    padding: 120px 16px;
+  }
+
+  .projects-title {
+    width: 100%;
+    max-width: 1440px;
+    margin-bottom: 40px;
+
+    h2 {
+      display: inline;
+      font-size: 32px;
+      background: #8A2387;
+      background: -webkit-linear-gradient(to right, #F27121, #E94057, #8A2387);
+      background: linear-gradient(to right, #F27121, #E94057, #8A2387);
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
+    }
+  }
+
+  .wrapper {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    max-width: 1440px;
+    gap: 8px;
+
+    @media (max-width: 1480px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media (max-width: 1160px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media (max-width: 800px) {
+      grid-template-columns: repeat(1, 1fr);
+    }
+
+    .project {
+      height: 320px;
+      background-color: rebeccapurple;
+    }
+  }
+`;
+
+export const ExperienceSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
   min-height: 100vh;
+  padding: 120px 32px;
+
+  @media (max-width: 800px) {
+    padding: 120px 16px;
+  }
 
   .wrapper {
     width: 100%;
     max-width: 1440px;
+
+    h2 {
+      display: inline;
+      font-size: 32px;
+
+      background: #8A2387;
+      background: -webkit-linear-gradient(to right, #F27121, #E94057, #8A2387);
+      background: linear-gradient(to right, #F27121, #E94057, #8A2387);
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
+    }
+
+    .experiences {
+      display: grid;
+      grid-template-columns: repeat(1, 1fr);
+      align-items: center;
+      gap: 32px;
+
+      span {
+        padding: 40px;
+        background-color: #09090B;
+        border-radius: 16px;
+        border: 1px solid #ffffff;
+        width: 100%;
+        gap: 16px;
+
+        @media (max-width: 800px) {
+          padding: 32px;
+        }
+
+        h3, h4, li, p {
+          font-family: "Poppins", sans-serif;
+        }
+
+        h3 {
+          margin-bottom: 8px;
+        }
+
+        h4 {
+          margin-bottom: 8px;
+        }
+      }
+
+      span:nth-child(1) {
+        margin-top: 40px;
+      }
+    }
   }
 `;
-
 
 export const SkillsSection = styled.section`
   display: flex;
@@ -108,13 +231,24 @@ export const SkillsSection = styled.section`
   padding: 120px 32px;
   width: 100%;
 
+  @media (max-width: 800px) {
+    padding: 120px 16px;
+  }
+
   .wrapper {
     width: 100%;
     max-width: 1440px;
 
     h2 {
       font-size: 32px;
-      margin-bottom: 40px;
+      display: inline;
+
+      background: #8A2387;
+      background: -webkit-linear-gradient(to right, #F27121, #E94057, #8A2387);
+      background: linear-gradient(to right, #F27121, #E94057, #8A2387);
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
     }
   }
 `;
@@ -123,6 +257,7 @@ export const SkillsContent = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 32px;
+  margin-top: 40px;
   
   @media (max-width: 1400px) {
     grid-template-columns: repeat(3, 1fr);
@@ -130,6 +265,10 @@ export const SkillsContent = styled.div`
 
   @media (max-width: 960px) {
     grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 800px) {
+    gap: 16px;
   }
 
   @media (max-width: 640px) {
@@ -144,6 +283,7 @@ export const SkillsContent = styled.div`
     border: 1px solid #ffffff;
     padding: 32px;
     gap: 16px;
+    background-color: #040405;
 
     p {
       font-family: 'Poppins', sans-serif;
@@ -183,6 +323,13 @@ export const ContactSection = styled.section`
       font-size: 6vw;
       text-align: center;
       margin-bottom: 140px;
+
+      background: #59C173;
+      background: -webkit-linear-gradient(to right, #5D26C1, #a17fe0, #59C173);
+      background: linear-gradient(to right, #5D26C1, #a17fe0, #59C173);
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
     }
 
     span {
