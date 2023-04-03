5dcb6625f2b6a432f1effd4f3ac4bd89763b3d8c
paulobr4z <paulobrazaraujo@gmail.com>
2023-04-03 17:02:28 -0300

    feat: add google analytics  test script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,6 @@
 import type { AppProps } from 'next/app'
 import { GlobalStyle } from '@/styles/global'
 
-import ReactGA from "react-ga";
-
-const TRACKING_ID = "G-QK2DB5G9KR";
-ReactGA.initialize(TRACKING_ID);
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,8 @@ import Document, {
   DocumentInitialProps
 } from 'next/document';
 
+import Script from 'next/script';
+
 export default class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
@@ -52,6 +54,18 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QK2DB5G9KR"></Script>
+          <Script>
+            {
+              `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-QK2DB5G9KR');`
+            }
+          </Script>
+
         </body>
       </Html>
     )
